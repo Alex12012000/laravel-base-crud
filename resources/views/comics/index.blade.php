28297e1fa1a376785e2e2c95f6327d9df261df00
Alex12012000 <arcex.29@gmail.com>
2022-08-31 16:24:53 +0200

    delete

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -13,13 +13,25 @@
             Price: {{$comic['price']}}
         </div>
 
+        {{-- detail page --}}
         <div>
             <a href="{{ route('comics.show', ['comic' => $comic->id]) }}">More details..</a>
         </div>
 
+
+        {{-- edit --}}
         <div>
             <a href="{{ route('comics.edit', ['comic' => $comic->id]) }}">Modifica prodotto</a>
         </div>
+
+        {{-- delete --}}
+        <div>
+            <form action="{{ route('comics.destroy', ['comic' => $comic->id]) }}" method="post">
+                @csrf
+                @method('DELETE')
+
+                <input type="submit" value="Cancella" onClick="return confirm('Sei sicuro di voler cancellare?');">
+            </form>
         <br>
     @endforeach
 @endsection
